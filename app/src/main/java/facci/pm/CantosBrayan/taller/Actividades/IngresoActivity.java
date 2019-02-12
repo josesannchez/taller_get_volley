17d4c906696089b242c07de1fb23acddd9e2e8b8
Brayan Cantos <brayancantosm07ci@gmail.com>
2019-02-12 11:52:16 -0500

    Cambios Actuales

---
+++ app/src/main/java/facci/pm/CantosBrayan/taller/Actividades/IngresoActivity.java
@@ -0,0 +1,15 @@
+package facci.pm.CantosBrayan.taller.Actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import facci.pm.CantosBrayan.taller.R;
+
+public class IngresoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso);
+    }
+}
