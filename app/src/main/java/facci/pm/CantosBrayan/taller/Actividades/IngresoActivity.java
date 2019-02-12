21f7e89ae49362beb2eeb95783ed3f5b062c8bc4
Brayan Cantos <brayancantosm07ci@gmail.com>
2019-02-12 12:33:07 -0500

    Cambios

--- app/src/main/java/facci/pm/CantosBrayan/taller/Actividades/IngresoActivity.java
+++ app/src/main/java/facci/pm/CantosBrayan/taller/Actividades/IngresoActivity.java
@@ -2,14 +2,25 @@ package facci.pm.CantosBrayan.taller.Actividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import facci.pm.CantosBrayan.taller.R;
 
 public class IngresoActivity extends AppCompatActivity {
-
+    private EditText id, nombre, estado, foto, descripcion, precio, marca;
+    private Button ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso);
+        id = (EditText)findViewById(R.id.TXTIdIngresar);
+        nombre = (EditText)findViewById(R.id.TXTNombreIngresar);
+        estado = (EditText)findViewById(R.id.TXTEstadoIngresar);
+        foto = (EditText)findViewById(R.id.TXTFotoIngresar);
+        descripcion = (EditText)findViewById(R.id.TXTDescripcionIngresar);
+        precio = (EditText)findViewById(R.id.TXTPrecioIngresar);
+        marca = (EditText)findViewById(R.id.TXTMarcaIngresar);
+        ingresar = (Button)findViewById(R.id.BTNIngresarProducto);
     }
 }
--- app/src/main/java/facci/pm/CantosBrayan/taller/Actividades/MainActivity.java
+++ app/src/main/java/facci/pm/CantosBrayan/taller/Actividades/MainActivity.java
@@ -1,10 +1,13 @@
 package facci.pm.CantosBrayan.taller.Actividades;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Productos> productosArrayList;
     private ProgressDialog progressDialog;
     private AdaptadorProductos adaptadorProductos;
+    private Button button;
 
 
     @Override
@@ -47,6 +51,13 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setMessage("Loading..");
         progressDialog.show();
         adaptadorProductos = new AdaptadorProductos(productosArrayList);
+        button = (Button)findViewById(R.id.BTNIngreso);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, IngresoActivity.class));
+            }
+        });
 
         DatosGenerales();
 
